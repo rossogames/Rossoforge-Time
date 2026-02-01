00afca395e493bf7a734901107e1088b93905459
Agustin Rosso <AgustinRosso_1@hotmail.com>
2026-02-01 19:34:15 +0100

    avances en timeflow

--- Runtime/Service/TimeFlowService.cs
+++ Runtime/Service/TimeFlowService.cs
@@ -1,9 +1,59 @@
+using Rossoforge.Core.Events;
+using Rossoforge.Core.Services;
 using Rossoforge.Core.TimeFlow;
+using Rossoforge.Services;
+using System;
+using UnityEngine;
 
-namespace Rossoforge.TimeFlow
+namespace Rossoforge.TimeFlow.Service
 {
-    public class TimeFlowService : ITimeFlowService
+    public class TimeFlowService : ITimeFlowService, IInitializable
     {
-        
+        private IEventService _eventService;
+        private float _previousTimeScale = 1f;
+
+        public bool IsPaused { get; private set; }
+
+        public void Initialize()
+        {
+            _eventService = ServiceLocator.Get<IEventService>();
+        }
+
+        public virtual DateTime GetCurrentTime()
+        {
+            return DateTime.UtcNow;
+        }
+
+        public void PauseTimeFlow()
+        {
+            if (IsPaused)
+                return;
+
+            _previousTimeScale = Time.timeScale;
+            SetTimeScale(0);
+
+            IsPaused = true;
+            _eventService.Raise<TimeFlowPausedEvent>();
+        }
+
+        public void ResumeTimeFlow()
+        {
+            if (!IsPaused)
+                return;
+
+            var targetScale = _previousTimeScale <= 0f ? 1f : _previousTimeScale;
+            SetTimeScale(targetScale);
+
+            IsPaused = false;
+            _eventService.Raise(new TimeFlowResumedEvent(targetScale));
+        }
+
+        private void SetTimeScale(float timeScale)
+        {
+            if (timeScale < 0f) 
+                timeScale = 0f;
+
+            Time.timeScale = timeScale;
+        }
     }
 }
